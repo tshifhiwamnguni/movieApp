ea88eb0618f2c6631ccea9cceaf7a08b547a36ea
shaggy <shaggy.sambo@dadevs.co.za>
2023-01-10 12:32:28 +0200

    sec

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
-import { lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css';
+import { lazy, Suspense } from 'react';
 import "./App.css";
 
 const Login = lazy(() => import("./components/login/login"));
+const Home = lazy(()=> import('./components/home/home'));
+
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <Suspense fallback={<div>Loading....</div>}>
+          <Routes>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='log' element={<Login/>}></Route>
+          </Routes>
+        </Suspense>
+      </Router>
     </div>
   );
 }
