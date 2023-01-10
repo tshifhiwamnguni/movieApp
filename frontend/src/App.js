1e5dd5360ef4da31edce99479ff9322d210c7c41
tshifhiwa <tshifhiwa.mnguni@dadevs.co.za>
2023-01-10 15:07:08 +0200

    forgot password?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 const Login = lazy(() => import("./components/login/login"));
 const Home = lazy(()=> import('./components/home/home'));
+const ForgotPassword = lazy(()=> import('./components/passwords/ForgotPassword'));
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home/>}></Route>
             <Route path='log' element={<Login/>}></Route>
+            <Route path='forgot' element={<ForgotPassword/>}></Route>
           </Routes>
         </Suspense>
       </Router>
