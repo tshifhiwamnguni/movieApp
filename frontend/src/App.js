a6a3abec3c832374dc2a42fd2f60bdacc29a5457
shaggy <shaggy.sambo@dadevs.co.za>
2023-01-10 10:57:32 +0200

    last

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import { lazy } from "react";
+import "./App.css";
 
+const Login = lazy(() => import("./components/login/login"));
 function App() {
   return (
     <div className="App">
-     
+      <Login />
     </div>
   );
 }
