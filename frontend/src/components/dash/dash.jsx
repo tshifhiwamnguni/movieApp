84418087ed16ece71f9f2f173cf6dcdc77d17bde
Fhatuwani <fhatuwani.raphalalani@dadevs.co.za>
2023-01-11 16:32:10 +0200

    initial commit

--- frontend/src/components/dash/dash.jsx
+++ frontend/src/components/dash/dash.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import womanKing from '../../../src/assets/womanKing.jpeg'
 function Dashboard() {
     return (
+      
       <container className="min-h-screen">
         <div className="grid gap-3 md:grid-cols-3 lg:grid-cols-3 sm:grid-cols-1 xs:grid-cols-1">
   
@@ -49,38 +50,9 @@ function Dashboard() {
             </div>
           </div>
   
-          {/* <div className="card w-96 bg-primary rounded text-primary-content mt-16">
-            <div className="card-body ">
-              <h2 className="card-title flex justify-center ">Past Events</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-  
-              </div>
-            </div>
-          </div>
-  
-          <div className="card w-96 bg-primary rounded text-primary-content mt-16">
-            <div className="card-body">
-              <h2 className="card-title flex justify-center ">Ongoing Events</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-  
-              </div>
-            </div>
-          </div>
-  
-          <div className="card w-96 bg-primary rounded text-primary-content mt-16">
-            <div className="card-body">
-              <h2 className="card-title flex justify-center ">Venue Events</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-  
-              </div>
-            </div>
-          </div> */}
-  
         </div>
       </container>
+
     );
   }
 
--- frontend/src/components/statistics/statistics.jsx
+++ frontend/src/components/statistics/statistics.jsx
@@ -1,17 +1,81 @@
-
+import {BsFillJournalBookmarkFill} from "react-icons/bs"
+import {BiMoviePlay} from "react-icons/bi"
+import {FaUsers} from "react-icons/fa"
+import {MdEventAvailable} from "react-icons/md"
 
 function Statistics() {
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
+    <>
+   
+
+ 
+    <container className="flex  justify-center">
+
+      
+      <div className="grid md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 lg:grid-cols-2 gap-3">
         
-        <div className="card-body">
-            <h2 className="card-title">Number of bookings</h2>
-            <p className="text-5xl font-bold">12345678</p>
-            <div className="card-actions justify-end">
-                <button className="btn btn-primary">View Now</button>
+       
+
+        <div className="card w-96 bg-primary text-primary-content mt-16">
+          <div className="card-body text-center">
+            <BsFillJournalBookmarkFill className="mx-auto text-6xl"/>
+            <h2 className="card-title flex justify-center">Number of Bookings</h2>
+            <div className="stat">
+                <p className="stat-value">123456789</p>
             </div>
+            
+            <div className="card-actions justify-end">
+                <button className="btn bg-base-400">View More</button>
+            </div>
+          </div>
         </div>
-    </div>
+
+        <div className="card w-96 bg-primary text-primary-content mt-16">
+          <div className="card-body text-center">
+            <BiMoviePlay className="mx-auto text-6xl"/>
+            <h2 className="card-title flex justify-center">Number of Movies</h2>
+            <div className="stat">
+                <p className="stat-value">6789</p>
+            </div>
+            
+            <div className="card-actions justify-end">
+                <button className="btn bg-base-400">View More</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card w-96 bg-primary text-primary-content mt-16">
+          <div className="card-body text-center">
+            <FaUsers className="mx-auto text-6xl"/>
+            <h2 className="card-title flex justify-center">Number of Users</h2>
+            <div className="stat">
+                <p className="stat-value">56789</p>
+            </div>
+            
+            <div className="card-actions justify-end">
+                <button className="btn bg-base-400">View More</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card w-96 bg-primary text-primary-content mt-16">
+          <div className="card-body text-center">
+            <MdEventAvailable className="mx-auto text-6xl"/>
+            <h2 className="card-title flex justify-center">Movies Available for Booking</h2>
+            <div className="stat">
+                <p className="stat-value">6789</p>
+            </div>
+            
+            <div className="card-actions justify-end">
+                <button className="btn bg-base-400">View More</button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </container>
+    </>
+
     );
 }
 
