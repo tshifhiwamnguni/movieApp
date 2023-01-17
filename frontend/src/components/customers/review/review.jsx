3581fa64b64df3431647618e865f682a2ef93c07
Fhatuwani <fhatuwani.raphalalani@dadevs.co.za>
2023-01-17 14:02:10 +0200

    customer review

--- frontend/src/components/customers/review/review.jsx
+++ frontend/src/components/customers/review/review.jsx
@@ -1,7 +1,46 @@
+import { useState, useEffect } from "react";
 import womenKing from "../../../assets/womanKing.jpeg"
-import StarRating from "../starrating/starrating"
+import axios from "axios";
 
 function Review() {
+    const [review, setReview] = useState('');
+    const [firstname, setFirstname] = useState('');
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+    let [movie, setMovie] = useState('') 
+  
+    const API = "https://strapi-movie-app.onrender.com/api";
+    console.log(rating);
+    console.log(review);
+    console.log(firstname);
+
+    axios.get(`${API}/movies`).then((response) => {
+        console.log(response.data.data[0].attributes.title)
+       let movie = response.data.data[0].attributes.title
+    }).catch((error) => {
+        console.log(error)
+    });
+
+    axios.get(`${API}/review-cinemas`).then((res) => {
+        console.log(res)
+    }).catch((err) => {
+        console.log(err)
+    });
+
+    
+    // axios.post(`${API}/review-cinemas`, {
+    //     id: 1,
+    //     rating: rating,
+    //     comment: review,
+    //     movie:"Pirates of the carribean"
+    //   })
+    //   .then(function (response) {
+    //     console.log(response);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
+
     return (
         <>
             <h2 className="h-12 w-auto text-center text-3xl font-bold text-gray-900 mt-8">Add Review</h2>
@@ -10,7 +49,7 @@ function Review() {
                 <div className="w-full max-w-md space-y-8">
                     <div className="flex flex-row gap-4 border-4 rounded-md px-4 py-4">
                         <img className="h-12 w-16" src={womenKing} alt="movie poster" />
-                        <h2 className="h-12 w-auto text-center text-xl font-bold text-gray-900">The Woman King</h2>
+                        <h2 className="h-12 w-auto text-center text-xl font-bold text-gray-900">Piedsrgtfarsd</h2>
 
                     </div>
                     <form className="mt-2 border-4 px-8 rounded-md py-8 space-y-6" action="#" method="POST">
@@ -19,17 +58,33 @@ function Review() {
                         <div className="flex gap-2 flex-col rounded-md shadow-sm">
                             <div>
                                 <label className="text-lg">Rating</label>
-                                <StarRating required />
+                                <div className="star-rating text-2xl">
+                                    {[...Array(5)].map((star, index) => {
+                                        index += 1;
+                                        return (
+                                            <button
+                                                type="button"
+                                                key={index}
+                                                className={index <= (hover || rating) ? "on" : "off"}
+                                                onClick={() => setRating(index)}
+                                                onMouseEnter={() => setHover(index)}
+                                                onMouseLeave={() => setHover(rating)}
+                                            >
+                                                <span className="star">&#9733;</span>
+                                            </button>
+                                        );
+                                    })}
+                                </div>
                             </div>
 
                             <div>
                                 <label className="text-lg">Review</label>
-                                <textarea id="review" rows="4" name="review" type="text" required class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="comments" />
+                                <textarea id="review" rows="4" name="review" type="text" required className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="comments" onChange={(e) => setReview(e.target.value)} value={review} />
                             </div>
 
                             <div>
                                 <label className="text-xl">Firstname</label>
-                                <input id="firstname" name="firstname" type="text" required class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="enter first name" />
+                                <input id="firstname" name="firstname" type="text" required className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="enter first name" onChange={(e) => setFirstname(e.target.value)} value={firstname} />
                             </div>
                         </div>
 
---
+++ frontend/src/components/customers/star_ratings/star_ratings.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./star_ratings.css"
+
+const StarRatings = () => {
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
+    console.log(rating);
+    return (
+        <div className="star-rating text-2xl">
+            {[...Array(5)].map((star, index) => {
+                index += 1;
+                return (
+                    <button
+                        type="button"
+                        key={index}
+                        className={index <= (hover || rating) ? "on" : "off"}
+                        onClick={() => setRating(index)}
+                        onMouseEnter={() => setHover(index)}
+                        onMouseLeave={() => setHover(rating)}
+                    >
+                        <span className="star">&#9733;</span>
+                    </button>
+                );
+            })}
+        </div>
+    );
+};
+
+export default StarRatings;
