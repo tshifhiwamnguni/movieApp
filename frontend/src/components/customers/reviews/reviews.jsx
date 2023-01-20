9229d1074e8ee533259aab6fd73dff3f9f8fa361
Fhatuwani <fhatuwani.raphalalani@dadevs.co.za>
2023-01-20 15:47:11 +0200

    reviews cards done

--- frontend/src/components/customers/reviews/reviews.jsx
+++ frontend/src/components/customers/reviews/reviews.jsx
@@ -1,21 +1,92 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import { CardSubtitle, CardText, CardTitle } from "reactstrap"
 import womanKing from "../../../assets/womanKing.jpeg"
 import { MdModeEditOutline, MdDelete } from 'react-icons/md';
 import axios from "axios"
 const API = "https://strapi-movie-app.onrender.com/api";
 
-const Reviews = () => {
-    const [reviews, setReviews]= useState([]);
 
-    function getReviews(){
-        axios.get(`${API}/review-cinemas`).then((response) => {
-            console.log(response.data);
-            
+const Reviews = () => {
+    const [reviews, setReviews] = useState([]);
+    const [stars, setStars] = useState();
+    let timestamp = "2023-01-17"
+    let comment = "Great movie"
+    let username = "Fhatuwani"
+    let movieTitle = "Avatar the lost city"
+    let index = 2;
+    let arr = []
+
+    const StarRatings = () => {
+        const [rating, setRating] = useState();
+        const [hover, setHover] = useState();
+        console.log(rating);
+        const parentToChild = () => {
+            setRating(rating)
+        }
+        return (
+            <div className="star-rating text-2xl">
+                {[...Array(5)].map((star, index) => {
+                    index += 1;
+                    return (
+                        <button
+                            type="button"
+                            key={index}
+                            className={index <= (hover || rating) ? "on" : "off"}
+                            onClick={() => parentToChild()}
+                            onMouseEnter={() => setHover(index)}
+                            onMouseLeave={() => setHover(rating)}>
+                            <span className="star">&#9733;</span>
+                        </button>
+                    );
+                })}
+            </div>
+        );
+    };
+
+    function UserOne() {
+        return <CardSubtitle tag="h5" >⭐ </CardSubtitle>;
+      } 
+      function UserTwo() {
+        return <CardSubtitle tag="h5" >⭐ ⭐ </CardSubtitle>;
+      } 
+      function UserThree() {
+        return <CardSubtitle tag="h5" >⭐ ⭐ ⭐ </CardSubtitle>;
+      } 
+      function UserFour() {
+        return <CardSubtitle tag="h5" >⭐ ⭐ ⭐ ⭐ </CardSubtitle>;
+      } 
+      function UserFive() {
+        return <CardSubtitle tag="h5" >⭐ ⭐ ⭐ ⭐ ⭐  </CardSubtitle>;
+      } 
+
+      function Greeting() {
+        let ratingNum = 5
+        if (ratingNum === 2) {
+          return <UserOne />;
+        }else if(ratingNum === 3){
+            return <UserThree />;
+        }else if(ratingNum === 4){
+            return <UserFour />;
+        }else if(ratingNum === 4){
+            return <UserFive />;
+        }else {
+            return <UserOne />;
+        }
+        
+      }
+
+    function getReviews() {
+        axios.get(`${API}/review-cinemas?populate=*`).then((response) => {
+            console.log(response.data.data);
+            arr = response.data.data
+
         }).catch((error) => {
             console.log(error);
         });
     }
-    
+
+   
+
     useEffect(() => {
         getReviews();
     })
@@ -28,71 +99,48 @@ const Reviews = () => {
 
                         <h1 className="text-4xl mx-auto text-center xl:text-2xl font-semibold leading-6 text-gray-800 py-8">Customer’s Cart</h1>
                         <div className="flex outline p-2 rounded flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full ">
-                       {reviews.map(() => {
 
-                        return (
-                            <>
+
                             <div className="w-full md:w-40">
                                 <img className="w-full hidden md:block" src={womanKing} alt="dress" />
                                 <img className="w-full md:hidden" src={womanKing} alt="dress" />
                             </div>
                             <div className="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full  pb-8">
                                 <div className="w-full flex flex-col justify-start items-start ">
-                                    <h3 className="text-lg text-center md:text-xl font-semibold leading-6 xl:leading-5 text-gray-800 py-1"> - Nu metro VIP @ Waterfront</h3>
+
+                                    <CardTitle tag="h1" className="text-center">{movieTitle} - Nu metro VIP @ Waterfront</CardTitle>
                                     <div className="rating py-1">
-                                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-                                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" defaultChecked />
-                                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-                                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-                                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+                                        {[...Array(stars || 1)].map((star, index) => {
+                                            return <CardSubtitle tag="h5" key={index}>⭐ </CardSubtitle>;
+                                        })}
                                     </div>
-                                    <h3 className="text-xl xl:text-md font-semibold leading-6 text-gray-800">Fhatuwani - 04/01/2023</h3>
-                                    <div className="flex justify-start items-start flex-col space-x-8 w-full py-2">
-                                        <p className="text-sm leading-none text-gray-800 ">
-                                            {}
-                                        </p>
+                                    <CardSubtitle className="mb-2 text-muted" tag="h6">
+                                        {username || "John Doe"}
+                                    </CardSubtitle>
+                                    <div className="reviews-body">
+                                        <CardText>"
+                                            {comment ||
+                                                "Lorem ipsum dolor sit amet consectetur adipisicing elit."}"
+                                        </CardText>
+                                        <CardText>
+                                            <small className="text-muted text-bold">
+                                                {timestamp || "3 mins ago"}
+                                            </small>
+                                        </CardText>
+                                        <Greeting />
+                                      
+                                       
                                     </div>
                                 </div>
+
                                 <div className="flex justify-end space-x-8 items-end w-full">
+                                    <MdDelete className="text-2xl text-rose-400" />
 
-                                    <p className="text-sm leading-none text-gray-800 flex gap-8"><MdModeEditOutline className="outline rounded text-2xl text-yellow-500" /> <MdDelete className="outline rounded text-2xl text-rose-500" /></p>
-                                </div>
-                            </div>
-                            </>
-                        );
-                       })}
-
-                        
-                            <div className="w-full md:w-40">
-                                <img className="w-full hidden md:block" src={womanKing} alt="dress" />
-                                <img className="w-full md:hidden" src={womanKing} alt="dress" />
-                            </div>
-                            <div className="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full  pb-8">
-                                <div className="w-full flex flex-col justify-start items-start ">
-                                    <h3 className="text-lg text-center md:text-xl font-semibold leading-6 xl:leading-5 text-gray-800 py-1">The Woman King - Nu metro VIP @ Waterfront</h3>
-                                    <div className="rating py-1">
-                                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-                                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" defaultChecked />
-                                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-                                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-                                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-                                    </div>
-                                    <h3 className="text-lg :text-md font-semibold leading-6 text-gray-800">Fhatuwani</h3>
-                                    <h4 className=" :text-md font-semibold leading-6 text-gray-800">04/01/2023</h4>
-                                    <div className="flex justify-start items-start flex-col space-x-8 w-full py-2">
-                                        <p className="text-sm leading-none text-gray-800 ">
-                                            What an amazing experience we had. We went to watch Star Wars: The Last Skywalker at the VIP cinema at the Waterfront in December. Before we went in, we ordered 3 Long Island Ice Teas that should be brought to us every hour. The manageress, Naso Mluma, personally took it on, and our drinks were brought into the cinema on time, and were delicious (so delicious we couldn't remember the end of the movie and had to watch it again!).
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="flex justify-end space-x-8 items-end w-full">
-
-                                    <p className="text-sm leading-none text-gray-800 flex gap-8"><MdModeEditOutline className="outline rounded text-2xl text-yellow-500" /> <MdDelete className="outline rounded text-2xl text-rose-500" /></p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                  
+
 
                 </div>
             </div>
