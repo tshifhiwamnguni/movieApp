43afeb0981bbb242a5fb9e70f631012df6a65ea9
shaggy <shaggy.sambo@dadevs.co.za>
2023-01-24 11:33:33 +0200

    now

--- frontend/src/components/Cinema/Reviews/Review.jsx
+++ frontend/src/components/Cinema/Reviews/Review.jsx
@@ -90,7 +90,7 @@ function Review() {
     <div className="hero min-h-screen mt-24">
       <div className="card card-compact bg-base-200 w-full shadow-xl">
         <div className="card-body">
-          <h3 className=" card-title font-bold text-4xl">Reviews</h3>
+          <h3 className=" card-title text-center font-bold text-4xl">Reviews</h3>
           <div className="flex justify-center">
             <div className="mt-5">
               {!loading ? (
