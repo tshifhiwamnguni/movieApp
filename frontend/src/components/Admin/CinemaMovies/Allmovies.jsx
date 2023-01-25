ff2e6f98a08e224782876b34466c6a53087fc513
shaggy <shaggy.sambo@dadevs.co.za>
2023-01-25 11:42:39 +0200

    search

--- frontend/src/components/Admin/CinemaMovies/Allmovies.jsx
+++ frontend/src/components/Admin/CinemaMovies/Allmovies.jsx
@@ -215,10 +215,64 @@ function AllMovies() {
       getMovies();
     }, [page]);
 
+    // search use effect
+
+  useEffect(() => {
+    setLoading(true);
+    if(query){
+    axios
+      .get(
+        `${API}/movies?populate=*&pagination[pageSize]=5&pagination[page]=${page}&filters[title][$containsi]=${query}`,
+        {
+          headers: {
+            Authorization: `Bearer ${TOKEN}`,
+          },
+        }
+      )
+      .then((movie) => {
+        setMovies(movie.data.data);
+  
+      })
+      .catch((error) => {})
+      .finally(() => setLoading(false));
+    }else{
+        getMovies();
+    }
+  }, [query]);
+
   return (
     <div className="min-h-screen mt-24 overflow-x-scroll">
       <ToastContainer />
+      <div className="flex">
       <h1 className="text-center font-bold text-3xl mb-4">Movies</h1>
+      <div className="form-control flex-1">
+          <div className="input-group justify-end">
+            <input
+              type="text"
+              placeholder="Search…"
+              className="input input-bordered"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <button className="btn btn-square">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        </div>
       <div className="overflow-x-auto w-full">
         {loading ? (
           <progress className="progress progress-primary w-full"></progress>
