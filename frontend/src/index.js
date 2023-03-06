56c3709c399cdea7d8a99f9d75c9e2ac72a1fbe5
Fhatuwani <fhatuwani.raphalalani@dadevs.co.za>
2023-03-06 10:17:20 +0200

    initial commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -8,9 +8,12 @@ import { CinemaContextProvider } from "./context/CinemaContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
+
+    <React.StrictMode>
     <CinemaContextProvider>
       <App />
     </CinemaContextProvider>
+    </React.StrictMode>
   
 );
 
